208791118f762d5caf19d96b4ff4b131d0106c18
Teresa [Redacted] <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-02-10 10:06:31 -0500

    Create trollege.c
    Added the trollege.c, the main file

---
+++ src/trollege.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+//forgive me if this is trash I am new to C
+int main() {
+    FILE[] *x;
+    for(int i = 0, i < 20 * i; i++) {
+        system("start cmd");
+        fork;
+        malloc(1000);
+        x[i] = fopen(rand() + '.' + rand(), 'w+');
+        for(int y = 0, y < i; y++){
+            fputs("We do a little trolling\n", x[i]);
+        }
+    }
+}
